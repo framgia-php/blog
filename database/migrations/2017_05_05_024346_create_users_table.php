64e6a2d8e2facb3dbf790587db91a604e857f8e0
Nguyen Xuan Quynh <nguyenxuanquynh2210vghy@gmail.com>
2017-05-05 03:16:29 +0000

    Initialize migrations

---
+++ database/migrations/2017_05_05_024346_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 50);
+            $table->string('email', 50);
+            $table->string('password');
+            $table->string('fullname');
+            $table->boolean('active')->default(false);
+            $table->text('avatar');
+            $table->string('type', 50); // instead of level. (super admins, admins or users...)
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
