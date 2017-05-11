8b216504873f4b4a9ec205cd074bef7fc2db7670
Nguyen Xuan Quynh <nguyenxuanquynh2210vghy@gmail.com>
2017-05-11 06:16:11 +0000

    Admin update delete categories

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }} | @yield('title')</title>
 
@@ -27,5 +28,28 @@
     </div>
 
     {{ Html::script(elixir('js/admin.min.js')) }}
+
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $(document).ready(function () {
+            $(document).on('click', '.btn-delete-resource', function (e) {
+                e.preventDefault();
+
+                if (confirm('Do you want to delete?')) {
+                    var route = $(this).attr('href');
+
+                    $.post(route, { _method: 'delete' })
+                        .done(function (response) {
+                            window.location.reload();
+                        });
+                }
+            });
+        });
+    </script>
 </body>
 </html>
